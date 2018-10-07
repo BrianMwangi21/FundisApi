89e6966f468af69d74a9e04746f610f475b0b865
KABIRU <kabiru@localhost.localdomain>
2018-10-07 18:33:55 -0400

    Configuring database

--- config/database.php
+++ config/database.php
@@ -1,5 +1,8 @@
 <?php
 
+$DATABASE_URL=parse_url('postgres://rwsehlqvjeezqq:13e4cc8f012f62111281dbe7da46dd546250eb30047d571eb01b299b4d3eb6eb@ec2-54-243-147-162.compute-1.amazonaws.com:5432/d5edtbd8e58cs4');
+
+
 return [
 
     /*
@@ -13,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,15 +59,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
